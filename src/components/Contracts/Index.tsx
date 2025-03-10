9afdaaac3ca8907c07bbf571ffd6848adabe9727
manojnamana <namanamanoj70@gmail.com>
2025-03-10 15:54:13 +0530

    bugs resolved

--- src/components/Contracts/Index.tsx
+++ src/components/Contracts/Index.tsx
@@ -1,11 +1,12 @@
-import { Data } from '@/demo'
+
+import { Data } from '@/utiles/data'
 import { Box, Paper, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 export const Contracts = () => {
   return (
     <Stack gap={2}>
-      {Data.map((item, index) =>
+      {Data.map((item) =>
       item.opportunitiesData.map((item, index) =>(
         <Paper elevation={3} key={index} sx={{padding: 2}}>
             <Box display={"flex"} alignItems={"center"} >
